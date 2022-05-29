4283138995f8881940d60759348463eef6262747
HamidZahid <hamidchaudhary3867@gmail.com>
2022-05-29 11:02:28 +0500

    M

---
+++ Transaction.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include("includes/header/header.php"); ?>
+    <title>Transaction</title>
+</head>
+
+<body>
+    <?php include("./includes/navbar/navbar.php"); ?>
+    <div class="container-fluid bg-light my-4">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <form>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Date Range</label>
+                                <input type="text" class="form-control" id="exampleInputPassword1" value="" data-id=""
+                                    placeholder="EXP-00009" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Account</label>
+                                <select id="disabledSelect" class="form-select" value="" data-id="">
+                                    <option value="">All</option>
+                                    <option value="">Choose</option>
+                                    <option value="">Choose</option>
+                                    <option value="">Choose</option>
+                                    <option value="">Choose</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Contact</label>
+                                <select id="disabledSelect" class="form-select">
+                                    <option value="">All</option>
+                                    <option value="">Choose</option>
+                                    <option value="">Choose</option>
+                                    <option value="">Choose</option>
+                                    <option value="">Choose</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Category</label>
+                                <select id="disabledSelect" class="form-select">
+                                    <option value="">All</option>
+                                    <option value="">Choose</option>
+                                    <option value="">Choose</option>
+                                    <option value="">Choose</option>
+                                    <option value="">Choose</option>
+                                </select>
+                            </div>
+                            <button type="button" class="btn btn-primary">Filter</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-body">
+                        <section class="p-2 rounded">
+                            <div class="container-fluid overflow-auto">
+                                <table class="display" id="myTable" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">ID</th>
+                                            <th scope="col">Date</th>
+                                            <th scope="col">Account</th>
+                                            <th scope="col">Type</th>
+                                            <th scope="col">Amount</th>
+                                            <th scope="col">Description</th>
+                                            <th scope="col">Dr.(USD)</th>
+                                            <th scope="col">Cr.(USD)</th>
+                                            <th scope="col">Manage  </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr href="javascript:void(0)" onclick="change_color(this)">
+                                            <td>1120</td>
+                                            <td>2022-05-25</td>
+                                            <td>Standard Chartered</td>
+                                            <td>In</td>
+                                            <td>$1,800.00</td>
+                                            <td>transfer for goods recieved</td>
+                                            <td>0.00</td>
+                                            <td>1800.00</td>
+                                            <td><button class=" btn btn-primary btn-outline-light"> <i class="fa-solid fa-file"></i></button></td>
+                                        </tr>
+                                        <tr href="javascript:void(0)" onclick="change_color(this)">
+                                            <td>1121</td>
+                                            <td>2022-05-25</td>
+                                            <td>JPMorgan Chase & Co.</td>
+                                            <td>Out</td>
+                                            <td>$1,800.00</td>
+                                            <td>transfer for goods recieved</td>
+                                            <td>0.00</td>
+                                            <td>1800.00</td>
+                                            <td><button class=" btn btn-primary btn-outline-light"> <i class="fa-solid fa-file"></i></button></td>
+                                        </tr>
+                                        <tr href="javascript:void(0)" onclick="change_color(this)">
+                                            <td>1122</td>
+                                            <td>2022-05-25</td>
+                                            <td>HSBC</td>
+                                            <td>Expense</td>
+                                            <td>$1,800.00</td>
+                                            <td>Phone</td>
+                                            <td>0.00</td>
+                                            <td>1800.00</td>
+                                            <td><button class=" btn btn-primary btn-outline-light"> <i class="fa-solid fa-file"></i></button></td>
+                                        </tr>
+                                        <tr href="javascript:void(0)" onclick="change_color(this)">
+                                            <td>1123</td>
+                                            <td>2022-05-25</td>
+                                            <td>City Bank</td>
+                                            <td>Expense</td>
+                                            <td>$1,800.00</td>
+                                            <td>Freight</td>
+                                            <td>0.00</td>
+                                            <td>1800.00</td>
+                                            <td><button class=" btn btn-primary btn-outline-light"> <i class="fa-solid fa-file"></i></button></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        $(document).ready(function () {
+            $("#myInput").on("keyup", function () {
+                var value = $(this).val().toLowerCase();
+                $("#dataTable tr").filter(function () {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+        $(document).ready(function () {
+            $("#myInput").on("keyup", function () {
+                var value = $(this).val().toLowerCase();
+                $("#dataTablee tr").filter(function () {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+
+    </script>
+
+    <script>
+        $(document).ready(function () {
+            $('#myTable').DataTable({
+                "bPaginate": true,
+                "bLengthChange": true,
+                "bFilter": false,
+                "bInfo": true,
+                "bAutoWidth": true,
+                // dom: 'Bfrtip',
+                // buttons: [
+                //     'copy', 'excel', 'pdf'
+                // ]
+            });
+        });
+    </script>
+</body>
+
+</html>
--- includes/header/header.php
+++ includes/header/header.php
@@ -1,19 +1,28 @@
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <!-- bootstrap link  -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <script src="assets/js/bootstrap.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-                    <!-- style css  -->
-    <link rel="stylesheet" href="styles/css/style.css">
-                <!-- fontawesome Icons cdn  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-                <!-- jquery  data table  -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- bootstrap link  -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.css" />
+<script src="assets/js/bootstrap.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"
+    integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js"
+    integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy"
+    crossorigin="anonymous"></script>
+<!-- style css  -->
+<link rel="stylesheet" href="styles/css/style.css">
+<!-- fontawesome Icons cdn  -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- jquery  data table  -->
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
