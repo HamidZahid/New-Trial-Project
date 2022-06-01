492bc330ceb035745c61e851541e1e64c00bc6e0
HamidZahid <hamidchaudhary3867@gmail.com>
2022-06-01 15:27:19 +0500

    M

---
+++ PaidExpenseInvoice.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include("./includes/header/header.php"); ?>
+    <title>Paid Expense Invoice</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="h4 fw-bold">Paid Invoices List</div>
+        <!-- breadcrumb   -->
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item text-decoration-none" id="" data-id=""><a href="#"
+                        style="text-decoration: none;">Dashboard</a></li>
+                <li class="breadcrumb-item active" aria-current="page" id="" data-id="">Paid Invoices List
+                </li>
+            </ol>
+        </nav>
+        <div class="card">
+            <div class="card-body">
+                <div class="h6 fw-bold mx-3" id="" data-id="">Exporting Paid-Expense Invoices Data</div>
+                <section class="p-2 rounded">
+                    <div class="container-fluid overflow-auto">
+                        <table class="display" id="myTable" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Invoice no</th>
+                                    <th scope="col">Created by. Name</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Total Amount</th>
+                                    <th scope="col">Paid Amount</th>
+                                    <th scope="col">Option</th>
+                                </tr>
+                            </thead>
+                            <tbody id="change">
+                                <tr href="javascript:void(0)" onclick="change_color(this)">
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>No Data Available in table</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>
+                                        <!-- <button class=" btn btn-success rounded-0"><i class="fa-solid fa-eye"></i>
+                                            View History</button>
+                                        <button class=" btn  rounded-0 mx-1" style="border: 1px solid black;"><i
+                                                class="fa-solid fa-pencil"></i> Edit</button> -->
+                                    </td>
+                                </tr>
+                            </tbody>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Invoice no</th>
+                                <th scope="col">Created by. Name</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Total Amount</th>
+                                <th scope="col">Paid Amount</th>
+                                <th scope="col">Option</th>
+                            </tr>
+                        </table>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+    <script>
+        $(document).ready(function () {
+            $("#myInput").on("keyup", function () {
+                var value = $(this).val().toLowerCase();
+                $("#dataTable tr").filter(function () {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+        $(document).ready(function () {
+            $("#myInput").on("keyup", function () {
+                var value = $(this).val().toLowerCase();
+                $("#dataTablee tr").filter(function () {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+
+    </script>
+    <script>
+        $(document).ready(function () {
+            $("#myInput").on("keyup", function () {
+                var value = $(this).val().toLowerCase();
+                $("#change tr").filter(function () {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+    </script>
+    <script>
+        $(document).ready(function () {
+            $('#myTable').DataTable({
+                "bPaginate": true,
+                "bLengthChange": true,
+                searching: true,
+                "bFilter": true,
+                "bInfo": true,
+                "bAutoWidth": true
+            });
+        });
+    </script>
+</body>
+
+</html>
